df5e6f5c24b62ab762d54d6f0790519fbf184aee
Peter Prigel <peterprigel0811@gmail.com>
2024-10-05 21:17:16 +0700

    Sub-state Setup : Hierarchical State Machine
    Sudah tersedia relasi Parent dan Child antara state dengan sub-state. Tetapi belum dicoba untuk dijalankan/debug.

--- Stock Rising/Assets/Scripts/State Machine/SemesterBaseState.cs
+++ Stock Rising/Assets/Scripts/State Machine/SemesterBaseState.cs
@@ -4,6 +4,8 @@ public abstract class SemesterBaseState
 {
     protected SemesterStateMachine _ctx;
     protected SemesterStateFactory _factory;
+    protected SemesterBaseState _currentSubState;
+    protected SemesterBaseState _currentSuperState;
     public SemesterBaseState(SemesterStateMachine currentContext, SemesterStateFactory semesterStateFactory)
     {
         _ctx = currentContext;
@@ -20,6 +22,9 @@ public abstract class SemesterBaseState
     {
 
     }
+
+    // SwitchState digunakan untuk berganti dari satu state ke state lain, bukan untuk SUB-STATE yaaa
+    // Untuk mengakses SUB-STATE fungsi yang digunakan adalah InitializeSubState(), UpdateStates(), SetSuperState(), SetSubState()
     protected void SwitchState(SemesterBaseState newState)
     {
         // current state exits state
@@ -29,13 +34,16 @@ public abstract class SemesterBaseState
         newState.EnterState();
         _ctx.CurrentState = newState;
     }
-    protected void SetSuperState()
-    {
 
+    // Fungsi SetSuperState() dan SetSubState() adalah untuk membuat relasi Parent dan Child yang timbal balik
+    protected void SetSuperState(SemesterBaseState newSuperState)
+    {
+        _currentSuperState = newSuperState;
     }
-    protected void SetSubState()
+    protected void SetSubState(SemesterBaseState newSubState)
     {
-
+        _currentSubState = newSubState;
+        newSubState.SetSuperState(this);
     }
 
 }
