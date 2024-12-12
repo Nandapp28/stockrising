622324e535e921467de863ff6c981b74097a5b1f
Peter Prigel <74586876+peterprigel@users.noreply.github.com>
2024-12-12 11:14:15 +0700

    Core Rumor Phase Mechanic is Done ✅
    Mekanik utama fase rumor telah selesai. Tetapi belum di set ganti ke fase resolusi.

--- Stock Rising/Assets/Scripts/Finite State Machine/RumorPhaseState.cs
+++ Stock Rising/Assets/Scripts/Finite State Machine/RumorPhaseState.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class RumorPhaseState : SemesterBaseState
 {
@@ -17,8 +18,11 @@ public class RumorPhaseState : SemesterBaseState
 
     GameObject board;
     BoardScript boardScript;
+
     string rumorCardTextureName;
     bool isTextureNameSaved = false;
+
+    bool isCoreRumorPhaseDone = false;
 
     public override void EnterState(SemesterStateManager semester)
     {
@@ -42,64 +46,111 @@ public class RumorPhaseState : SemesterBaseState
                     // referensi ke boardscript 
                     board = GameObject.Find("Red Board");
                     boardScript = board.GetComponent<BoardScript>();
-
-                    // move kamera
-                    GameObject camPost = board.transform.Find("Camera Post").gameObject;
-                    isMoving = true;
-                    if (isMoving == true)
-                    {
-                        MoveCamera(semester.mainCamera, camPost);
-                        isMoving = false;
-                    }
-
-                    DelaySession1();
-
-                    if (delaySession1Done == true)
-                    {
-                        // referensi ke kartu rumor paling atas
-                        Transform rumorCards = board.transform.Find("Rumor Cards");
-                        int rumorCardsTotal = rumorCards.childCount;
-                        GameObject rumorCard = rumorCards.Find("Rumor Card " + rumorCardsTotal).gameObject;
-                        GameObject card = rumorCard.transform.Find("Card").gameObject;
-                        RumorCardScript rumorCardScript = card.GetComponent<RumorCardScript>();
-
-                        // angkat kartu selama 10 detik
-                        if (timeOpenRumorCard >= 0)
-                        {
-                            timeOpenRumorCard -= Time.deltaTime;
-                            rumorCardScript.isTaked = true;
-                        }
-                        else
-                        {
-                            if (isTextureNameSaved == false)
-                            {
-                                rumorCardTextureName = card.GetComponent<Renderer>().material.mainTexture.name;
-                                isTextureNameSaved = true;
-                                rumorCardScript.isTaked = false;
-                                rumorCard.SetActive(false);
-                            }
-                        }
-                    }
-
-                    // terapkan efek
-                    if (isTextureNameSaved == true)
-                    {
-                        Debug.Log(rumorCardTextureName);
-                    }
+                    CoreRumorPhase(semester, boardScript);
 
                     break;
 
                 case GameState.Player2Turn:
                     Debug.Log("Player 2's Turn " + "Sekarang adalah giliran ");
+                    board = GameObject.Find("Orange Board");
+                    boardScript = board.GetComponent<BoardScript>();
+                    CoreRumorPhase(semester, boardScript);
                     break;
 
                 case GameState.Player3Turn:
                     Debug.Log("Player 3's Turn " + "Sekarang adalah giliran ");
+                    board = GameObject.Find("Blue Board");
+                    boardScript = board.GetComponent<BoardScript>();
+                    CoreRumorPhase(semester, boardScript);
                     break;
 
                 case GameState.PlayersStop:
-                    Debug.Log("Ganti Fase Rumor");
+                    Debug.Log("PlayerStop's turn");
+                    board = GameObject.Find("Green Board");
+                    boardScript = board.GetComponent<BoardScript>();
+                    CoreRumorPhase(semester, boardScript);
                     break;
+            }
+        }
+    }
+
+    private void CoreRumorPhase(SemesterStateManager semester, BoardScript boardScript)
+    {
+        // move kamera
+        GameObject camPost = board.transform.Find("Camera Post").gameObject;
+        isMoving = true;
+        if (isMoving == true)
+        {
+            MoveCamera(semester.mainCamera, camPost);
+            isMoving = false;
+        }
+
+        DelaySession1();
+
+        // referensi ke kartu rumor paling atas
+        Transform rumorCards = board.transform.Find("Rumor Cards");
+        int rumorCardsTotal = rumorCards.childCount;
+        GameObject rumorCard = rumorCards.Find("Rumor Card " + rumorCardsTotal).gameObject;
+        GameObject card = rumorCard.transform.Find("Card").gameObject;
+        RumorCardScript rumorCardScript = card.GetComponent<RumorCardScript>();
+        if (delaySession1Done == true)
+        {
+            // angkat kartu selama 10 detik
+            if (timeOpenRumorCard >= 0)
+            {
+                timeOpenRumorCard -= Time.deltaTime;
+                rumorCardScript.isTaked = true;
+            }
+            else
+            {
+                if (isTextureNameSaved == false)
+                {
+                    rumorCardTextureName = card.GetComponent<Renderer>().material.mainTexture.name;
+                    isTextureNameSaved = true;
+                    rumorCardScript.isTaked = false;
+                    rumorCard.SetActive(false);
+                }
+                DelaySession2();
+            }
+        }
+
+        if (delaySession2Done == true && isTextureNameSaved == true)
+        {
+            // terapkan efek
+            if (isCoreRumorPhaseDone == false)
+            {
+                Debug.Log(rumorCardTextureName);
+                RumorCardDB(boardScript, semester);
+                isCoreRumorPhaseDone = true;
+            }
+            DelaySession3();
+        }
+
+        if (delaySession3Done == true)
+        {
+            Debug.Log("Ganti state selanjutnya");
+
+            isTextureNameSaved = false;
+
+            timeOpenRumorCard = 5.0f;
+            delaySession1Done = false;
+            timeDelaySession1 = 1.5f;
+            delaySession2Done = false;
+            timeDelaySession2 = 1.0f;
+            delaySession3Done = false;
+            timeDelaySession3 = 2.5f;
+            isCoreRumorPhaseDone = false;
+
+            semester.DestroyObject(rumorCard);
+
+            if (semester.playerState == (GameState)3)
+            {
+                Debug.Log("Ganti Fase Resolusi");
+                //semester.SwitchState(semester.rumorPhase);
+                semester.SwitchPlayerState();
+            } else
+            {
+                semester.SwitchPlayerState();
             }
         }
     }
@@ -119,7 +170,7 @@ public class RumorPhaseState : SemesterBaseState
         }
     }
 
-    private void RumorCardDB(PlayerScript playerScript, SemesterStateManager semester) // sekali main
+    private void RumorCardDB(BoardScript boardScript, SemesterStateManager semester) // sekali main
     {
         // ambil nilai nama texture yang disimpan di variabel savedActionCardTakenTexture
         string textureName = rumorCardTextureName;
@@ -127,11 +178,35 @@ public class RumorPhaseState : SemesterBaseState
         // membuat dynamic dictionary
         Dictionary<Func<string, bool>, Action> dynamicActions = new Dictionary<Func<string, bool>, Action>
         {
-            { name => name.EndsWith("FB"), () => {
-                //FlashbuyFx();
+            { name => name == "uv_KR1", () => { 
+                AppliedRumor(boardScript, 2);
             } },
-            { name => name.EndsWith("IT"), () => {
-                //InsiderTradeFx();
+            { name => name == "uv_KR2", () => {
+                AppliedRumor(boardScript, 2);;
+            } },
+            { name => name == "uv_KR3", () => {
+                AppliedRumor(boardScript, -1);;
+            } },
+            { name => name == "uv_KR4", () => {
+                AppliedRumor(boardScript, -1);;
+            } },
+            { name => name == "uv_KR5", () => {
+                AppliedRumor(boardScript, 3);
+            } },
+            { name => name == "uv_KR6", () => {
+                AppliedRumor(boardScript, 3);
+            } },
+            { name => name == "uv_KR7", () => {
+                AppliedRumor(boardScript, -3);
+            } },
+            { name => name == "uv_KR8", () => {
+                AppliedRumor(boardScript, -3);
+            } },
+            { name => name == "uv_KR9", () => {
+                AppliedRumor(boardScript, -2);
+            } },
+            { name => name == "uv_KR10", () => {
+                AppliedRumor(boardScript, -2);
             } },
         };
 
@@ -146,7 +221,13 @@ public class RumorPhaseState : SemesterBaseState
         }
     }
 
-    float timeDelaySession1 = 2.5f;
+    private static void AppliedRumor(BoardScript boardScript, int i)
+    {
+        int stockCoordinate = i;
+        boardScript.currentStockCoordinate += stockCoordinate;
+    }
+
+    float timeDelaySession1 = 1.5f;
     bool delaySession1Done = false;
     bool DelaySession1()
     {
@@ -159,6 +240,38 @@ public class RumorPhaseState : SemesterBaseState
         }
 
         return delaySession1Done = false;
+    }
+
+    float timeDelaySession2 = 1.0f;
+    bool delaySession2Done = false;
+    bool DelaySession2()
+    {
+        if (timeDelaySession2 >= 0)
+        {
+            timeDelaySession2 -= Time.deltaTime;
+        }
+        else
+        {
+            return delaySession2Done = true;
+        }
+
+        return delaySession2Done = false;
+    }
+
+    float timeDelaySession3 = 2.5f;
+    bool delaySession3Done = false;
+    bool DelaySession3()
+    {
+        if (timeDelaySession3 >= 0)
+        {
+            timeDelaySession3 -= Time.deltaTime;
+        }
+        else
+        {
+            return delaySession3Done = true;
+        }
+
+        return delaySession3Done = false;
     }
 
     void MoveCamera(Camera mainCam, GameObject camPost2)
